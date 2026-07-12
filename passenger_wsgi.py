639683c1b398b2bf7cf65b5e52a94e90386a1c86
Zarem <m1.zare1369@gmail.com>
2026-07-12 09:00:56 +0430

    Fix Passenger production settings

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -5,6 +5,6 @@ BASE_DIR = os.path.dirname(__file__)
 
 sys.path.insert(0, BASE_DIR)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
 from config.wsgi import application
