080345f8ad5644bdf664676ca2fe2f4546dffec6
Zarem <m1.zare1369@gmail.com>
2026-07-21 09:27:46 +0430

    change url.py in config

--- config/urls.py
+++ config/urls.py
@@ -1,32 +1,17 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib.sitemaps.views import sitemap
 from products.sitemaps import ProductSitemap
-
 from django.http import HttpResponse
+
 
 sitemaps = {
     "products": ProductSitemap,
 }
+
 
 def robots_txt(request):
     return HttpResponse(
@@ -36,17 +21,22 @@ def robots_txt(request):
         content_type="text/plain",
     )
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-   path("", include("products.urls")),
-     path("orders/", include("orders.urls")),
-     path(
-    "sitemap.xml",
-    sitemap,
-    {"sitemaps": sitemaps},
+    path("admin/", admin.site.urls),
+
+    path("", include("products.urls")),
+    path("orders/", include("orders.urls")),
+
     path("robots.txt", robots_txt),
-    name="django.contrib.sitemaps.views.sitemap",
-),
+
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="sitemap",
+    ),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
